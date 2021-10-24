8b23c610058872549be5ec27621b48d5d7046f9e
Diego Galante <diegogalantte@gmail.com>
2021-10-24 10:59:21 -0300

    Initial

---
+++ src/Controllers/TypingHardFunctionsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FuncAzureTypingHard.Controllers
+{
+    internal class TypingHardFunctionsController
+    {
+
+        [FunctionName("ping")]
+        public IActionResult Ping(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        ILogger log)
+        {
+            return new OkObjectResult("Azure Functions TypingHard - Ok - Ping");
+        }
+
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,14 @@
+﻿using FuncAzureTypingHard;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace FuncAzureTypingHard
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+        }
+    }
+}
