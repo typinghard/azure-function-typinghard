d8d1f60d80160967b38f45dbeeaf8b658a6e6df6
Diego Galante <diegogalantte@gmail.com>
2021-12-09 19:09:42 -0300

    Add new function

--- src/Controllers/TypingHardFunctionsController.cs
+++ src/Controllers/TypingHardFunctionsController.cs
@@ -1,13 +1,45 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AzureDevopsTracker.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
 
 namespace FuncAzureTypingHard.Controllers
 {
     internal class TypingHardFunctionsController
     {
+        private readonly ChangeLogService _changeLogService;
+
+        public TypingHardFunctionsController(ChangeLogService changeLogService)
+        {
+            _changeLogService = changeLogService;
+        }
+
+        [FunctionName("count-items-for-release")]
+        public IActionResult CountItemsForRelease(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        ILogger log)
+        {
+            try
+            {
+                var count = _changeLogService.CountItemsForRelease();
+
+                var returnText = new StringBuilder();
+                if (count == 0 || count == 1)
+                    returnText.Append($"{count} Item");
+                else
+                    returnText.Append($"{count} Items");
+
+                return new OkObjectResult(new { data = returnText.ToString() });
+            }
+            catch (Exception ex)
+            {
+                return new OkObjectResult(new { data = ex.Message });
+            }
+        }
 
         [FunctionName("ping")]
         public IActionResult Ping(
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,5 +1,9 @@
-﻿using FuncAzureTypingHard;
+﻿using AzureDevopsTracker.Configurations;
+using AzureDevopsTracker.Data;
+using AzureDevopsTracker.Services;
+using FuncAzureTypingHard;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(Startup))]
 namespace FuncAzureTypingHard
@@ -8,7 +12,11 @@ namespace FuncAzureTypingHard
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            var configuration = builder.GetContext().Configuration;
 
+            builder.Services.AddScoped<ChangeLogService>();
+            
+            builder.Services.AddAzureDevopsTracker(new DataBaseConfig(configuration["ConnectionStrings:DefaultConnection"], "adst"));
         }
     }
 }
